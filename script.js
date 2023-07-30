ebd5403624efdbc40fec1fa9680781c49b77d0a6
Gerardo <gerardoulinsutuj@gmail.com>
2023-07-30 09:19:25 -0600

    Formulario de DW

---
+++ script.js
@@ -0,0 +1,9 @@
+function guardarDatos() {
+    var nombre = document.getElementById('nombre').value;
+    var email = document.getElementById('email').value;
+  
+    // Aquí puedes agregar código para guardar los datos en una base de datos o hacer alguna otra acción con ellos
+    // En este ejemplo, simplemente mostraremos una alerta con los datos ingresados:
+    alert('Nombre: ' + nombre + '\nCorreo Electrónico: ' + email);
+  }
+  
